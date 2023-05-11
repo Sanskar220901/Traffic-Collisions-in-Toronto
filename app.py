c0b83e85acb05e96096c30e6b407a3cb14e38167
Sanskar220901 <68878408+Sanskar220901@users.noreply.github.com>
2023-05-11 00:12:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import pydeck as pdk
 import numpy as np
-import plotly.express as px
+import plotly as px
 import plotly.graph_objects as go
 
 st.set_page_config(layout='wide')
