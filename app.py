389a4225f2c485e2d84cabfde2b8cf35638adedf
Sanskar220901 <68878408+Sanskar220901@users.noreply.github.com>
2023-05-13 00:20:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -284,6 +284,6 @@ st.markdown("<hr>", unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: center;'> References: </h5>", unsafe_allow_html=True)
 
 
-st.markdown("<a href = 'https://data.torontopolice.on.ca/datasets/TorontoPS::ksi/about'> Toronto Police Service - Public Safety Data Portal </a>", unsafe_allow_html=True)
+st.markdown("<a style='text-align: center; href = 'https://data.torontopolice.on.ca/datasets/TorontoPS::ksi/about'> Toronto Police Service - Public Safety Data Portal </a>", unsafe_allow_html=True)
 
 
